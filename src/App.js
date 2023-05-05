690646af770cc6000c7de62e4fa7f319a62f4fb2
jutionck <jutionck@gmail.com>
2023-05-05 10:09:25 +0700

    add: jsx

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo-enigma-color.svg";
+import "./App.css";
+
+function getName() {
+  return "Enigma Camp";
+}
 
 function App() {
+  const bidang = "IT Bootcamp";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>React JSX</h1>
+      {/* <img src="logo192.png" />
+      <img src={logo} width={imageSize} /> */}
+
+      <h1>Biodata Perusahaan</h1>
+      <ul>
+        <li>Nama: {getName()}</li>
+        <li>Bidang: {bidang}</li>
+        <img src={logo} alt="Enigma Logo" />
+      </ul>
+    </>
   );
 }
 
